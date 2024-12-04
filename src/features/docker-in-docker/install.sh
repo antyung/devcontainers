8a8854f8c65fc9e8e4e2bfce34563cb5028005c3
Anthony Yung <antyung@outlook.sg>
2024-12-04 20:18:30 +0800

    Add files

--- src/features/docker-in-docker/install.sh
+++ src/features/docker-in-docker/install.sh
@@ -4,7 +4,7 @@ set -o errexit -o pipefail
 
 USERNAME="${USERNAME:-"${_REMOTE_USER:-"vscode"}"}"
 
-function install_apt() {
+function install_apt_ubuntu() {
     $(which sudo) apt-get update
     export DEBIAN_FRONTEND=noninteractive
     $(which sudo) apt-get install -y --no-install-recommends sudo ca-certificates curl
@@ -13,6 +13,24 @@ function install_apt() {
     sudo chmod a+r /etc/apt/keyrings/docker.asc
     echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    apt-get autoremove -y
+    apt-get clean -y
+    rm -rf /var/lib/apt/lists/*
+}
+
+function install_apt_debian() {
+    $(which sudo) apt-get update
+    export DEBIAN_FRONTEND=noninteractive
+    $(which sudo) apt-get install -y --no-install-recommends sudo ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
@@ -142,8 +160,15 @@ EOF
     sudo usermod -aG docker "${USERNAME}"
 }
 
-function main() {
-    install_apt
+main() {
+    if grep -qi "ubuntu" /etc/os-release; then
+        install_apt_ubuntu
+    elif grep -qi "debian" /etc/os-release; then
+        install_apt_debian
+    else
+        echo "Unsupported OS. Exiting."
+        exit 1
+    fi
     setup_docker_init
 }
 
