dd57aa39f6f563d37d4338458202ef3dad03dd4b
Anthony Yung <antyung@outlook.sg>
2024-12-18 14:19:57 +0800

    Add files

---
+++ src/features/nodejs/install.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -o errexit -o pipefail
+
+VERSION=${VERSION:-"20.18.1"}
+
+function install_apt() {
+    ARCH="$(uname -m)"
+    case ${ARCH} in
+        x86_64) ARCH="linux-x64";;
+        aarch64 | armv8*) ARCH="linux-arm64";;
+        *) echo "(!) Architecture ${ARCH} unsupported"; exit 1 ;;
+    esac
+    $(which sudo) apt-get update
+    export DEBIAN_FRONTEND=noninteractive
+    $(which sudo) apt-get install -y --no-install-recommends sudo wget ca-certificates xz-utils
+    sudo apt-get clean
+    sudo rm -rf /var/lib/apt/lists/*
+}
+
+function install_node() {
+    wget -q "https://nodejs.org/dist/v${VERSION}/node-v${VERSION}-${ARCH}.tar.xz" -P /tmp
+    tar -xvf "node-v${VERSION}-${ARCH}.tar.xz" -C /tmp
+    mv /tmp/node-v${VERSION}-${ARCH}/bin/* /usr/local/bin
+    mv /tmp/node-v${VERSION}-${ARCH}/lib/* /usr/local/lib
+    rm -rf "/tmp/node-v${VERSION}-${ARCH}" "/tmp/node-v${VERSION}-${ARCH}.tar.xz"
+    corepack enable
+    corepack prepare pnpm@latest --activate
+}
+
+function main() {
+    install_apt
+    install_node
+}
+
+main
