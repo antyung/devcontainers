743ddbe263fa1b2a187b78d051f9101741fb4889
Anthony Yung <antyung@outlook.sg>
2025-01-01 05:41:25 +0800

    Add files

--- src/features/docker-outside-of-docker/install.sh
+++ src/features/docker-outside-of-docker/install.sh
@@ -17,6 +17,7 @@ function install_apt_ubuntu() {
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
     sudo apt-get install -y --no-install-recommends docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo usermod -aG docker "${USERNAME}"
 }
 
 function install_apt_debian() {
